fa901e30a57cd4c9111ac6c4e4e5cd9ec22bd552
tpavay <pavayt@gmail.com>
2025-04-29 09:40:20 -0400

    Initial Commit

---
+++ BotTalk/BotTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotTalkApp.swift
+//  BotTalk
+//
+//  Created by Tyler Pavay on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BotTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BotTalkTests/BotTalkTests.swift
@@ -0,0 +1,17 @@
+//
+//  BotTalkTests.swift
+//  BotTalkTests
+//
+//  Created by Tyler Pavay on 4/29/25.
+//
+
+import Testing
+@testable import BotTalk
+
+struct BotTalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
